3168b2e15837eae4f9526ebf7618efed44337c1d
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-04-23 14:21:22 +0700

    membuat akses modifier

--- OOP1.cpp
+++ OOP1.cpp
@@ -20,3 +20,14 @@ class Matakuliah {
 		string kodeMK;
 		string namaMK;
 		int sks;
+
+        public:
+		void input() {
+			cout << "Kode MK: ";
+			cin >> kodeMK;
+			cout << "Nama MK: ";
+			cin >> namaMK;
+			cout << "SKS: ";
+			cin >> sks;
+		}
+
